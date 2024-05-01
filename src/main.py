fcc1e630bedceb22c8964f8365d0419f3d5e14f3
Jensen Holm <holmj@mail.gvsu.edu>
2024-05-01 11:38:39 -0400

    - using custom version of pybaseball plot for plotting - getting unique mlb filmroom url that filters mlb filmroom for the   pitch

--- src/main.py
+++ src/main.py
@@ -3,10 +3,20 @@ import numpy as np
 import pybaseball
 import datetime
 
+from plot_tunnel import plot_strike_zone
+from matplotlib import axes
+
+
+YEAR = str(datetime.date.today().year)
+MONTH = str(datetime.date.today().month)
+YESTERDAY = datetime.date.today() - datetime.timedelta(days=1)
+
 
 def _get_yesterdays_pitches() -> pl.DataFrame:
-    yesterday = str(datetime.date.today() - datetime.timedelta(days=1))
-    return pl.from_pandas(pybaseball.statcast(start_dt=yesterday, end_dt=yesterday))
+    return pl.from_pandas(pybaseball.statcast(
+        start_dt=str(YESTERDAY),
+        end_dt=str(YESTERDAY),
+    ))
 
 
 def _tie_pitches_to_previous(pitches_df: pl.DataFrame) -> pl.DataFrame:
@@ -21,7 +31,21 @@ def _tie_pitches_to_previous(pitches_df: pl.DataFrame) -> pl.DataFrame:
         )
     return sorted_pitches
 
-def _compute_tunnel_plus(statcast_pitches_df: pl.DataFrame) -> pl.DataFrame:
+
+def _get_player_names(pitches_df: pl.DataFrame) -> pl.DataFrame:
+    pitchers=pybaseball.playerid_reverse_lookup(
+        [pitcher["pitcher"] for pitcher in pitches_df.iter_rows(named=True)],
+        key_type="mlbam",
+    )
+    pitchers["name"] = (pitchers["name_first"] + " " + pitchers["name_last"]).str.title()
+
+    return pitches_df.join(
+        other=pl.from_pandas(pitchers[["key_mlbam", "name"]]),
+        left_on="pitcher",
+        right_on="key_mlbam",
+    )
+
+def _compute_tunnel_score(statcast_pitches_df: pl.DataFrame) -> pl.DataFrame:
     def _euclidean_distance(point1: tuple[pl.Expr, ...], point2: tuple[pl.Expr, ...]):
         x1, y1 = point1
         x2, y2 = point2
@@ -51,13 +75,72 @@ def _compute_tunnel_plus(statcast_pitches_df: pl.DataFrame) -> pl.DataFrame:
             point1=(pl.col("release_pos_x"), pl.col("release_pos_z")),
             point2=(pl.col("prev_release_pos_x"), pl.col("release_pos_z")),
         )
+    ).with_columns(
+        tunnel_score=pl.col("actual_distance") / pl.col("tunnel_distance"),
     )
+
+
+def _plot_pitches(tunneled_pitch: pl.DataFrame) -> axes.Axes:
+    # input should be a polars dataframe with just one pitch
+    # and it s previous one
+    
+    p1 = tunneled_pitch.select(
+        "game_date",
+        "at_bat_number",
+        "pitch_number",
+        "pitch_type",
+        "pitch_name",
+        "plate_x",
+        "plate_z",
+        "plate_x_no_movement",
+        "plate_z_no_movement",
+    )
+
+    pitch2 = tunneled_pitch.select(
+        "game_date",
+        "at_bat_number",
+        "prev_pitch_number",
+        "prev_pitch_type",
+        "prev_pitch_name",
+        "prev_plate_x",
+        "prev_plate_z",
+        "plate_x_no_movement",
+        "plate_z_no_movement",
+    )
+
+    p2 = pitch2.rename({
+        col: "_".join(col.split("_")[1:]) if col.startswith("prev") else col for col in pitch2.columns
+    })
+
+    tunnel_score = tunneled_pitch.select("tunnel_score").item()
+    print(tunneled_pitch.select("tunnel_distance").item())
+    pitcher = tunneled_pitch.select("name").item() 
+    df = pitch2.join(other=pl.concat([p1, p2,]), on=["game_date", "at_bat_number"]).to_pandas()
+
+    fig = plot_strike_zone(
+        data=df,
+        title=f"Best Pitch Yesterday by Tunnel Score\n{pitcher} {tunnel_score:.2f}",
+        colorby="pitch_name",
+        annotation="pitch_number",
+    )
+    return fig
+
+
+def _get_film_room_video(pitch: pl.DataFrame) -> str:
+    inning = pitch.select("inning").item()
+    top_bot = pitch.select("inning_topbot").item() # needs to be either TOP or BOT
+    balls = pitch.select("balls").item()
+    strikes = pitch.select("strikes").item()
+    pitcher_id = pitch.select("pitcher").item()
+    outs = pitch.select("outs_when_up").item()
+    url = f"https://www.mlb.com/video/?q=Season+%3D+%5B{YEAR}%5D+AND+Date+%3D+%5B%22{YESTERDAY}%22%5D+AND+PitcherId+%3D+%5B{pitcher_id}%5D+AND+TopBottom+%3D+%5B%22{top_bot.upper()}%22%5D+AND+Outs+%3D+%5B{outs}%5D+AND+Balls+%3D+%5B{balls}%5D+AND+Strikes+%3D+%5B{strikes}%5D+AND+Inning+%3D+%5B{inning}%5D+Order+By+Timestamp+DESC"
+    return url
 
 
 def main() -> None:
     yesterdays_df: pl.DataFrame = _get_yesterdays_pitches()
     tied_df: pl.DataFrame = _tie_pitches_to_previous(yesterdays_df)
-    tunnel_df: pl.DataFrame = _compute_tunnel_plus(tied_df)
+    tunnel_df: pl.DataFrame = _compute_tunnel_score(tied_df)
 
     keeper_cols: list[str] = [
         "pitcher",
@@ -81,6 +164,21 @@ def main() -> None:
         "game_date",
         "tunnel_distance",
         "actual_distance",
+        "p_throws",
+        "stand",
+        "inning_topbot",
+        "plate_x",
+        "plate_z",
+        "plate_z_no_movement",
+        "plate_x_no_movement",
+        "prev_plate_x",
+        "prev_plate_z",
+        "prev_plate_z_no_movement",
+        "prev_plate_x_no_movement",
+        "tunnel_score",
+        "at_bat_number",
+        "pitch_number",
+        "prev_pitch_number",
     ]
 
     # drop missing values from tunnel_df
@@ -92,13 +190,12 @@ def main() -> None:
         tunnel_df
         .drop_nulls(subset=keeper_cols)
         .select(keeper_cols)
-        .sort(by="tunnel_distance", descending=False)
-        .head(50)
-        .sort(by="actual_distance", descending=True)
+        .sort("tunnel_score", descending=True)
     )
 
-    print(tunnel_df.head())
-
+    tunnel_df = _get_player_names(tunnel_df)
+    fig = _plot_pitches(tunnel_df.head(1))
+    print(_get_film_room_video(pitch=tunnel_df.head(1)))
 
 if __name__ == "__main__":
     main()
