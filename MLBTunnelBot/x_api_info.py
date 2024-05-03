708026b4eab11adbb89aad2d34788214cda64db5
Jensen Holm <holmj@mail.gvsu.edu>
2024-05-02 23:52:22 -0400

    using environment variables instead of hard coded api keys

---
+++ MLBTunnelBot/x_api_info.py
@@ -0,0 +1,27 @@
+import tweepy
+import os
+
+CLIENT_ID = os.environ.get("CLIENT_ID")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+BEARER_TOKEN = os.environ.get("BEARER_TOKEN")
+CONSUMER_KEY = os.environ.get("CONSUMER_KEY")
+CONSUMER_SECRET = os.environ.get("CONSUMER_SECRET")
+ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.environ.get("ACCESS_TOKEN_SECRET")
+
+
+_auth = tweepy.OAuth1UserHandler(
+    consumer_key=CONSUMER_KEY,
+    consumer_secret=CONSUMER_SECRET,
+    access_token=ACCESS_TOKEN,
+    access_token_secret=ACCESS_TOKEN_SECRET,
+)
+
+api = tweepy.API(_auth)
+
+client = tweepy.Client(
+    consumer_key=CONSUMER_KEY,
+    consumer_secret=CONSUMER_SECRET,
+    access_token=ACCESS_TOKEN,
+    access_token_secret=ACCESS_TOKEN_SECRET,
+)
